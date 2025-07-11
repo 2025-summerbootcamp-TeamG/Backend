4f09fe3abe443fd935b024df3327f17d90b345f3
hyyeeon <gpdus8549@gmail.com>
2025-07-11 20:45:48 +0900

    데베 구축용 앱별 models.py

--- events/models.py
+++ events/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Event(models.Model):
+    artist = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+    genre = models.CharField(max_length=30)
+    age_rating = models.CharField(max_length=20)
+    max_reserve = models.IntegerField(null=True, blank=True)
+    image_url = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    view_count = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'event'  # 테이블 이름을 'user'로 지정
+
+
+class EventTime(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    event_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'event_time'  # 테이블 이름을 'user'로 지정
+
+
+class Zone(models.Model):
+    event_time = models.ForeignKey(EventTime, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    rank = models.CharField(max_length=5)
+    total_count = models.IntegerField()
+    available_count = models.IntegerField()
+
+    class Meta:
+        db_table = 'zone'  # 테이블 이름을 'user'로 지정
+
+
+class Seat(models.Model):
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
+    seat_number = models.CharField(max_length=10)
+    seat_status = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'seat'  # 테이블 이름을 'user'로 지정
--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'user'  # 테이블 이름을 'user'로 지정
