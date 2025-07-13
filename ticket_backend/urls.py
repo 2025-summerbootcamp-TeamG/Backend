080b63b135e2a872e206c224f62ec28d4d6673df
hyyeeon <gpdus8549@gmail.com>
2025-07-14 00:06:02 +0900

    티켓 예매 api

--- ticket_backend/urls.py
+++ ticket_backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from events.views import BuyTicketsView  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('events/<int:event_id>/tickets/buy', BuyTicketsView.as_view()),
 ]
